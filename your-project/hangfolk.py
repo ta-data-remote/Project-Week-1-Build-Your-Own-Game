1dbd1cc06485061389f0f2bbbbc1d3add7c77c5a
lauratll <lauratll@gmail.com>
2021-06-03 16:34:12 +0200

    changed code and gitignore file added

--- your-project/hangfolk.py
+++ your-project/hangfolk.py
@@ -1,4 +1,16 @@
 
+def player_input_letter(letters) : #list of letters
+    while True:
+        input_letter = input("Please,enter a letter: ")
+        if input_letter not in letters and input_letter.isdigit() == False:
+            print("Hola")
+            break
+        elif input_letter in letters:
+            print("Please,.....")
+        elif input_letter.isdigit():
+            print("Please....")
+
+    return input_letter
 
 
 
@@ -15,45 +27,31 @@ def print_current_game(used_letters,encrypted_word):
     print("Those are the letters you have used:", used_letters)
     print("This is the word:", encrypted_word)
 
-def player_input_letter():
-    input_letter = input("Please,enter a letter: ")
-    return input_letter
-
-def player_win(word,used_letters):
-    for character in word:
-        if character not in used_letters:
-            return False
-    return True
-
-def player_loose(lives):
-    if lives == 0:
-        return True
-    else:
-        False
 
 
-def print_result(player_won, word):
-    if player_won:
+def main():
+    ongoing = True
+    used_letters = []
+    lives = 11
+    word = "terrible"
+    player_wins = False
+
+    while ongoing:
+        player_letter = player_input_letter()
+        used_letters.append(player_letter)
+        word_display = generate_word_display(word, used_letters)
+        if player_letter not in word:
+            lives -= 1
+            if lives == 0:
+                ongoing = False
+
+        if "*" not in word_display:
+            ongoing= False
+            player_wins= True
+
+        print_current_game(used_letters, word_display)
+
+    if player_wins:
         print(f"Cool! You have won! The word was: {word}")
     else:
         print("Sorry, you have used all your lives. You are hanged!")
-
-ongoing = True
-used_letters = []
-lives = 11
-player_won= False
-word = "terrible"
-while ongoing:
-    word_display = generate_word_display(word,used_letters)
-    print_current_game(used_letters, word_display)
-    player_letter = player_input_letter()
-    used_letters.append(player_letter)
-    if player_letter in word:
-        if player_win(word, used_letters): #a letter from the word is not in the list of letters
-            ongoing = False
-            player_won = True
-    else:
-        lives -= 1
-        if player_loose(lives):
-            ongoing = False
-print_result(player_won,word)
